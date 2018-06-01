ce7ac0951c57087716e893a26450973a39e1f78f
Alex Joffroy <alexjoffroy@users.noreply.github.com>
2018-06-01 09:01:44 +0200

    Add LaravelPageCacheValetDriver.php

---
+++ LaravelPageCacheValetDriver.php
@@ -0,0 +1,39 @@
+<?php
+
+class LaravelPageCacheValetDriver extends LaravelValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return parent::serves($sitePath, $siteName, $uri) && 
+            is_dir("$sitePath/public/page-cache");
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        if ($uri === '/') {
+            $uri = 'pc__index__pc';
+        }
+
+        if (file_exists($cachePath = $sitePath.'/public/page-cache/'.$uri.'.html')) {
+            return $cachePath;
+        }
+
+        return parent::frontControllerPath($sitePath, $siteName, $uri);
+    }
+}
